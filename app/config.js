994639d3546c292831a46d174f7b5dd44afcdab9
Mark Esplana <>
2018-10-23 00:02:27 +0800

    aaa

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://18.136.183.193:3030"
+export const baseUrl = "http://148.72.207.72:3030"
 //export const baseUrl = "http://localhost:3030"
 //export const baseUrl = "http://el-chapo.herokuapp.com"
